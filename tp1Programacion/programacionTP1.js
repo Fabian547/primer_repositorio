a985ed458349c593bec9ef4d25c5bb880d6eb49f
Fabian547 <szadorskifabian@gmail.com>
2025-04-26 22:37:47 -0300

    Add files via upload

---
+++ tp1Programacion/programacionTP1.js
@@ -0,0 +1,46 @@
+document.getElementById("formulario").addEventListener(`submit`,agregartarea);
+const tareas=[]
+const prioridades={"alta":1,"media":2,"baja":3}
+function agregartarea(event){
+event.preventDefault();
+
+const nombre=document.getElementById("nombre").value;
+const prioridad=document.getElementById('prioridad').value;
+const categoria=document.getElementById('categoria').value;
+
+const tarea = { nombre, prioridad, categoria };
+    tareas.push(tarea);
+    tareas.sort((a, b) => prioridades[a.prioridad] - prioridades[b.prioridad]);
+    
+    renderizarTarjetas();
+
+    this.reset();
+}
+
+function renderizarTarjetas() {
+    const contenedor = document.getElementById("contenedor");
+    contenedor.innerHTML = "";
+
+    tareas.forEach((tarea, index) => {
+      const tarjeta = document.createElement("div");
+      tarjeta.className = "tarjeta";
+      tarjeta.innerHTML = `
+        <strong>Tarea:</strong> ${tarea.nombre}<br>
+        <strong>Categoría:</strong> ${tarea.categoria}<br>
+        <strong>Prioridad:</strong> ${tarea.prioridad}<br>
+        <label><strong>Estado:</strong></label>
+        <select>
+          <option>Pendiente</option>
+          <option>En proceso</option>
+          <option>Finalizado</option>
+        </select><br>
+        <button onclick="eliminarTarea(${index})">Borrar</button>
+      `;
+      contenedor.appendChild(tarjeta);
+    });
+  }
+  function eliminarTarea(indice) {
+    tareas = tareas.filter((_, i) => i !== indice);
+    renderizarTarjetas();
+  }
+
